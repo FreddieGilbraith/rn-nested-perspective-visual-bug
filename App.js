bd9e18600b0f084dddb7b9faa9f65d0038bf96a5
Freddie Gilbraith <freddie.gilbraith@littlebonsai.co.uk>
2022-01-14 15:41:14 +0000

    created minimal reproduction

--- App.js
+++ App.js
@@ -1,11 +1,83 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        flex: 1,
+        height: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          transform: [{ translateX: 10 }, { perspective: 1000 }],
+        }}
+      >
+        <Text>Single View</Text>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          transform: [{ translateX: 10 }],
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            transform: [{ translateY: 10 }, { perspective: 1000 }],
+          }}
+        >
+          <Text>Inner With Perspective</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          transform: [{ translateX: 10 }, { perspective: 1000 }],
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            transform: [{ translateY: 10 }],
+          }}
+        >
+          <Text>Outer With Perspective</Text>
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          alignItems: "center",
+          justifyContent: "center",
+          transform: [{ translateX: 10 }, { perspective: 1000 }],
+        }}
+      >
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "center",
+            transform: [{ translateY: 10 }, { perspective: 1000 }],
+          }}
+        >
+          <Text>Both With Perspective</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +85,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
